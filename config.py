3d3f81fe755e54377d9d0d9a48b103d1fda1c63b
tetoavbot <141924154+tetoavbot@users.noreply.github.com>
2023-08-25 14:17:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://graph.org/file/6e5004198c3bdbba84bdc.jpg")
-START_IMG = getenv("START_IMG", "https://graph.org/file/6e5004198c3bdbba84bdc.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/9cd60e6de8355a8866eda.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/9cd60e6de8355a8866eda.jpg")
 
 SESSION = getenv("SESSION", None)
 
@@ -24,4 +24,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/T7_AU")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5656828413").split()))
 
 
-FAILED = "https://graph.org/file/6e5004198c3bdbba84bdc.jpg"
+FAILED = "https://telegra.ph/file/9cd60e6de8355a8866eda.jpg"
