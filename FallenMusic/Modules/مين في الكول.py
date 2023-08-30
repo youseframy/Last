87b3dd6c96e31e4c778dab3b2992c135a3423c0d
tetoavbot <141924154+tetoavbot@users.noreply.github.com>
2023-08-30 18:21:55 +0300

    Add files via upload

---
+++ FallenMusic/Modules/مين في الكول.py
@@ -0,0 +1,66 @@
+import asyncio
+from pyrogram import filters, Client
+from YukkiMusic import app
+from YukkiMusic.utils.database import *
+from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError)
+from YukkiMusic.core.call import Yukki
+from pytgcalls import PyTgCalls, StreamType
+from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+
+@app.on_message(filters.regex("^مين في الكول$|^مين ف الكول$|^مين في كول$"))
+async def strcall(client, message):
+    assistant = await group_assistant(Yukki,message.chat.id)
+    try:
+        await assistant.join_group_call(message.chat.id, AudioPiped("./assets/mody.mp3"), stream_type=StreamType().pulse_stream)
+        text="🔔 الاعضاء المتواجدين في الكول :\n\n"
+        participants = await assistant.get_participants(message.chat.id)
+        k =0
+        for participant in participants:
+            info = participant
+            if info.muted == False:
+                mut=f"٭{participant.volume}٭يتحدث 🗣٭"
+            else:
+                mut="٭ساكت 🔕٭"
+            user = await client.get_users(participant.user_id)
+            print(participant.user_id)
+            k +=1
+            text +=f"{k}➤{user.mention}{mut}\n"
+        text += f"\nعددهم : {len(participants)}\n✔️"    
+        await message.reply(f"{text}")
+        await asyncio.sleep(5)
+        await assistant.leave_group_call(message.chat.id)
+    except NoActiveGroupCall:
+        await message.reply(f"عمووووو الكول مش مفتوح اصلااا\n❌")
+    except AlreadyJoinedError:
+        text="🔔 الاعضاء المتواجدين في الكول :\n\n"
+        participants = await assistant.get_participants(message.chat.id)
+        k =0
+        for participant in participants:
+            info = participant
+            if info.muted == False:
+                mut=f"٭{participant.volume}٭يتحدث 🗣٭"
+            else:
+                mut="٭ساكت 🔕٭"
+            user = await client.get_users(participant.user_id)
+            print(participant.user_id)
+            k +=1
+            text +=f"{k}➤{user.mention}{mut}\n"
+        text += f"\nعددهم : {len(participants)}\n✔️"
+        await message.reply(f"{text}")
+    except TelegramServerError:
+        await message.reply(f"ارسل الامر تاني في مشكله في سيرفر التلجرام\n❌")
+        
+        
+#المطور محمد هلال
+
+#™ تيتو
+
+# ســورس الفراعنة 
+
+# 🔥← المطور :  @G_7_Rr
+
+# 🐉← القناه  : @T7_AU
+
+# 💨← جروب : @T7_AU
+
+# 🤖← بوتي : @N7TTbot
